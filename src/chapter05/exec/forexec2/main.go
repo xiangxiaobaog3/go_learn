0362cd44fa428f766f114b67d73987207893f4b7
xiangxiaobao <943300866@qq.com>
2019-02-03 20:01:26 +0800

    for 练习题 嵌套两层循环

--- src/chapter05/exec/forexec2/main.go
+++ src/chapter05/exec/forexec2/main.go
@@ -5,10 +5,12 @@ import "fmt"
 func main() {
 	//统计3个班级成绩情况，每个班有5名同学，
 	//求出各个班的平均分和所有班级的平均分【学生的成绩从键盘输入】
+	var classNum int = 3
+	var stuNum int = 5
 	var totalSum float64 = 0.0
-	for j := 1; j <= 3; j++ {
+	for j := 1; j <= classNum; j++ {
 		sum := 0.0
-		for i := 1; i <= 5; i++ {
+		for i := 1; i <= stuNum; i++ {
 			var score float64
 			fmt.Printf("请输入第%d班 第%d个学生的成绩 \n", j, i)
 			fmt.Scanln(&score)
@@ -16,9 +18,9 @@ func main() {
 			sum += score
 		}
 
-		fmt.Printf("第%d个班级的平均分是%v \n", j, sum/5)
+		fmt.Printf("第%d个班级的平均分是%v \n", j, sum/float64(stuNum))
 		totalSum += sum
 	}
 
-	fmt.Printf("各个班的总成绩%v 所有班级平均分是 \n", totalSum, totalSum/(3*5))
+	fmt.Printf("各个班的总成绩%v 所有班级平均分是 \n", totalSum, totalSum/float64(classNum*stuNum))
 }
